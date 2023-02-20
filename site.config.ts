ce859867d2c476ea7d8fa21b4a163cded2498df1
Edward-Elric233 <45136205+Edward-Elric233@users.noreply.github.com>
2023-02-20 12:27:32 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'GAMES101-cc3aa9deb7e54c55bd2e7cedd4181f31',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'GAMES101 Lecture Notes',
+  domain: 'https://wangjiangkui.notion.site/GAMES101-cc3aa9deb7e54c55bd2e7cedd4181f31',
+  author: 'Edward-Elric233',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  twitter: '',
+  github: 'Edward-Elric233',
   linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
